c79a4e46e8c60682fca1ac4dece6fb944fe10cfe
Narongsak Phungdee <narongsn@ais.co.th>
2017-09-26 10:21:09 +0700

    Initial Commit

---
+++ DailyMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyMoney
+//
+//  Created by Narongsak Phungdee on 9/26/2560 BE.
+//  Copyright © 2560 Narongsak Phungdee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
